3ea1cdbba846ff63e57492140dc7588bb4bbd6dd
Oscar J Soto <oscar.j.soto12@gmail.com>
2024-02-17 18:16:12 -0500

    Neovim LSP Treesitter undotree etc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,26 +1,2 @@
-require("oscar")
-
--- Function to reload Lua files in specified directories
-function reload()
-    -- Reload files in .config/nvim/plugin directory
-    local plugin_path = vim.fn.expand('~/.config/nvim/plugin')
-    for _, file in ipairs(vim.fn.readdir(plugin_path)) do
-        if file:sub(-4) == '.lua' then
-            local plugin_file = plugin_path .. '/' .. file
-            package.loaded[plugin_file] = nil
-            dofile(plugin_file)
-        end
-    end
-
-    -- Reload files in .config/nvim/lua/user/ directory
-    local user_path = vim.fn.expand('~/.config/nvim/lua/oscar')
-    for _, file in ipairs(vim.fn.readdir(user_path)) do
-        if file:sub(-4) == '.lua' then
-            local user_file = user_path .. '/' .. file
-            package.loaded[user_file] = nil
-            dofile(user_file)
-        end
-    end
-
-end
+require("osoto")
 
--- nvim/.config/nvim/lua/osoto/set.lua
+++ nvim/.config/nvim/lua/osoto/set.lua
@@ -1,37 +1,32 @@
--- Visual Setting
 vim.opt.guicursor = ""
-vim.opt.termguicolors = true
-vim.opt.colorcolumn = "80"
-vim.opt.signcolumn = "yes"
 
--- Add Relative Number
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
--- Track Cursor
-vim.opt.scrolloff = 8
-
--- Tab Settings
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.smartindent = true
 
--- Remove Word Wrap
 vim.opt.wrap = false
 
--- Remove Backup
 vim.opt.swapfile = false
 vim.opt.backup = false
-
--- Add Undo Function
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
--- Search Settings
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
--- Performance
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
 vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
